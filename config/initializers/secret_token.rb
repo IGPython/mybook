517936f0524eb53bf62fb1dbbdcfd48535dd4f28
Rhys Park <sales@igpython.co.uk>
2013-08-20 22:56:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBook::Application.config.secret_token = '54d6be73a20e3fb461363dd48f22773ca21a17f974eafd1d5f53257b24aedbd3b68da0f84fb7bc41b126e758167cd11abc75dce5dee7131811fbfcca67066190'
