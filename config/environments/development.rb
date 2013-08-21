171a9ae564cdb6f7c800f2866f86ff7b017e21f3
Rhys Park <sales@igpython.co.uk>
2013-08-21 15:14:43 +0100

    Install devise and views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ MyBook::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #Devise Setup
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+
 end
