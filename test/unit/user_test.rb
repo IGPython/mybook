6f603c15eac45483095682b9529f969e7d6e5700
Rhys Park <sales@igpython.co.uk>
2013-08-22 13:35:59 +0100

    added test for username and profile corectness

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "a user should enter a first name" do 
+  	user= User.new
+  	assert !user.save	
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a profile name should not be blank" do
+  	user= User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user has entered his last name" do
+  	user= User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "user should have a unique profile name" do
+  	user= User.new
+  	user.profile_name = users(:testuser).profile_name
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "no spaces in profile name" do
+  	user = User.new
+  	user.profile_name = "My new name"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
 end
+
